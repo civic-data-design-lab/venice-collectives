78f8b19b80180226cd34dc07a75e553a2a3ad103
Dylan <d_h@mit.edu>
2020-01-30 16:03:27 -0500

    Basic Isotope implementation
    Only uses openness -- simple example for isotope layout and filtering for exact value. Added p tag `.openness` for each box, and parse text to int during init for isotope. Added listener for changes in openness slider.

--- index.js
+++ index.js
@@ -11,8 +11,31 @@ $('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
+$('document').ready( function (){
+  // init isotope
+  var grid = new Isotope( '#card-grid', {
+    // options
+      itemSelector: '.flex-card',
+      getSortData: {
+        openness: '.openness parseInt'
+      }
+    });
+
+  $('#open-closed').click(function(){
+    let open_filter = $(this).val()
+    console.log(open_filter)
+
+    grid.arrange({ filter: function() {
+        var number = $(this).find('.openness').text();
+        console.log
+        return parseInt( number, 10 ) == open_filter;
+      } 
+    })
+  })
+  
+});
 // element argument can be a selector string
 //   for an individual element
 // var iso = new Isotope( '.grid', {
 //   // options
-// });
+// })
