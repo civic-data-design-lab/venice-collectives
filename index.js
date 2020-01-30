e2845dfef559e7b6df2ea575c0a8a297acc9939c
Dylan <d_h@mit.edu>
2020-01-30 16:24:37 -0500

    Range check
    Added +/- 5 to range, plus dumb code fix

--- index.js
+++ index.js
@@ -16,24 +16,24 @@ $('document').ready( function (){
   var grid = new Isotope( '#card-grid', {
     // options
       itemSelector: '.flex-card',
+      order: 'random',
       getSortData: {
         openness: '.openness parseInt'
       }
     });
 
   $('#open-closed').click(function(){
-    let open_filter = $(this).val()
-    console.log(open_filter)
-
+    let open_filter = parseInt($(this).val())
+    
     grid.arrange({ filter: function() {
         var number = $(this).find('.openness').text();
-        console.log
-        return parseInt( number, 10 ) == open_filter;
+        return (parseInt( number, 10 ) < open_filter + 5) && (parseInt( number, 10 ) > open_filter - 5) ; 
       } 
     })
   })
-  
+
 });
+
 // element argument can be a selector string
 //   for an individual element
 // var iso = new Isotope( '.grid', {
