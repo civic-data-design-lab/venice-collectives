866e51c519f681ef46b994dacb87dae0e0c28df6
cotequotey <awang8248@gmail.com>
2020-02-05 22:03:02 -0500

    Filters work manually

--- index.js
+++ index.js
@@ -1,41 +1,87 @@
-// var elem = document.querySelector('.grid');
-// var iso = new Isotope( elem, {
-//   // options
-//   itemSelector: '.grid-item',
-//   layoutMode: 'fitColumns'
-// });
-
+// Dropdown for each slider definition
 $('ul.nav li.dropdown').hover(function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
+
+//Isotope integration to filter cards with sliders
 $('document').ready( function (){
   // init isotope
   var grid = new Isotope( '#card-grid', {
     // options
       itemSelector: '.flex-card',
+      layoutMode: 'fitRows',
       order: 'random',
       getSortData: {
-        openness: '.openness parseInt'
+        openness: '.openness parseInt',  
+        completeness: '.completeness parseInt',
+        size: '.size parseInt',
+        analogDigital: '.analogDigital parseInt',
+        centralization: '.centralization parseInt',
       }
     });
 
+  /* Open-closed filter */
   $('#open-closed').click(function(){
     let open_filter = parseInt($(this).val())
     
     grid.arrange({ filter: function() {
         var number = $(this).find('.openness').text();
+        // Only returns values 5 numbers greater or lower than chosen one (range 0-30)
         return (parseInt( number, 10 ) < open_filter + 5) && (parseInt( number, 10 ) > open_filter - 5) ; 
       } 
     })
   })
 
+  /* Minimal-holistic filter */
+  $('#minimal-holistic').click(function(){
+    let complete_filter = parseInt($(this).val())
+    
+    grid.arrange({ filter: function() {
+        var number = $(this).find('.completeness').text();
+        // Only returns values 5 numbers greater or lower than chosen one (range 0-30)
+        return (parseInt( number, 10 ) < complete_filter + 10) && (parseInt( number, 10 ) > complete_filter - 10) ; 
+      } 
+    })
+  })
+
+    /* Sited-global filter */
+    $('#sited-global').click(function(){
+      let size_filter = parseInt($(this).val())
+      
+      grid.arrange({ filter: function() {
+          var number = $(this).find('.size').text();
+          // Only returns values 5 numbers greater or lower than chosen one (range 0-30)
+          return (parseInt( number, 10 ) < size_filter + 10) && (parseInt( number, 10 ) > size_filter - 10) ; 
+        } 
+      })
+    })
+
+      /* Offline-online filter */
+    $('#offline-online').click(function(){
+    let analogDigital_filter = parseInt($(this).val())
+    
+    grid.arrange({ filter: function() {
+        var number = $(this).find('.analogDigital').text();
+        // Only returns values 5 numbers greater or lower than chosen one (range 0-30)
+        return (parseInt( number, 10 ) < analogDigital_filter + 5) && (parseInt( number, 10 ) > analogDigital_filter - 5) ; 
+      } 
+    })
+  })
+
+       /* Decentralized-singleLeader filter */
+       $('#decentralized-singleLeader').click(function(){
+        let centralization_filter = parseInt($(this).val())
+        
+        grid.arrange({ filter: function() {
+            var number = $(this).find('.centralization').text();
+            // Only returns values 5 numbers greater or lower than chosen one (range 0-30)
+            return (parseInt( number, 10 ) < centralization_filter + 5) && (parseInt( number, 10 ) > centralization_filter - 5) ; 
+          } 
+        })
+      })
+
 });
 
-// element argument can be a selector string
-//   for an individual element
-// var iso = new Isotope( '.grid', {
-//   // options
-// })
